2252a93599b20cc116600a922f9695c0c50de737
Xaviery Bonson Padaoan <xbpadaoan@gmail.com>
2026-05-09 03:01:35 +0800

    Polish Network Game Assignment

--- Assets/Script/MultiplayerMenu.cs
+++ Assets/Script/MultiplayerMenu.cs
@@ -1,25 +1,51 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.UI;
+using TMPro; 
 
-public class MultiplayerMenu : MonoBehaviour
+public class MultiplayerMenu : NetworkBehaviour
 {
+    [SerializeField] private GameObject menuPanel; 
+    [SerializeField] private TextMeshProUGUI playerCountText; 
     void Awake()
     {
         // Limits CPU usage by capping frames per second
         Application.targetFrameRate = 60;
     }
-
-    public void StartHost() //starts the game as both server and client, allowing the player to host a game and play on it
+    void Update()
+    {
+        if(IsServer)
+        {
+            int playerCount = NetworkManager.Singleton.ConnectedClients.Count;
+            UpdatePlayerCountRpc(playerCount); 
+        }
+    }
+    [Rpc(SendTo.Everyone)] // This attribute indicates that this method is a Remote Procedure Call (RPC) that should be executed on all clients, this is used to update the player count UI on all clients whenever there is a change in the number of connected players, ensuring that all players have accurate information about the current player count in the game
+    private void UpdatePlayerCountRpc(int playerCount)
+    {
+        if(NetworkManager.Singleton != null)
+        {
+            playerCountText.text = $"Players Count: {playerCount}";
+        }
+    }
+    public void StartHost() 
     {
         NetworkManager.Singleton.StartHost();
+        HideMenu();    
     }
 
-    public void StartClient() //starts the game as a client, allowing the player to join a game hosted by another player
+    public void StartClient() 
     {
         NetworkManager.Singleton.StartClient();
+        HideMenu(); 
     }
-    public void StartServer() //starts the game as a server, allowing the player to host a game but not play on it, useful for dedicated servers
+    public void StartServer() 
     {
         NetworkManager.Singleton.StartServer();
+        HideMenu(); 
+    }
+    public void HideMenu() 
+    {
+        menuPanel.SetActive(false);
     }
 }
